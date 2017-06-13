ca42e3385354c20fba6ae155b5e5e6088e425daf
foolish_hourse <442196841@qq.com>
2017-06-13 20:07:11 +0800

    项目初始化

---
+++ NetRequest_Demo/NetRequest_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetRequest_Demo
+//
+//  Created by YZL on 17/6/13.
+//  Copyright © 2017年 YZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetRequest_Demo/NetRequest_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetRequest_Demo
+//
+//  Created by YZL on 17/6/13.
+//  Copyright © 2017年 YZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
